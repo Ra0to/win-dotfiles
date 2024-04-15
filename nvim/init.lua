23b082ac855f44b9dd6ae3b5edfe41e6256f9ee9
Mikhail Batishchev <mikl181@yandex.ru>
2024-04-15 22:17:40 +0300

    revert semicolumn binding

--- nvim/init.lua
+++ nvim/init.lua
@@ -50,11 +50,6 @@ vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
 
-vim.keymap.set("n", ";", ":")
-vim.keymap.set("n", ":", ";")
-vim.keymap.set("v", ";", ":")
-vim.keymap.set("v", ":", ";")
-
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
