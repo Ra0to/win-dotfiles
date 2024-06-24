12b8aa991c77ed059c0879bc9ed9d6b28d713e62
Mikhail Batishchev <mikl181@yandex.ru>
2024-06-24 09:59:17 +0200

    add ideavimrc

--- scripts/create-symlinks.ps1
+++ scripts/create-symlinks.ps1
@@ -1,2 +1,3 @@
 New-Item -ItemType SymbolicLink -Path "$HOME\AppData\local\nvim\init.lua" -Target "$HOME\.configs\nvim\init.lua"
 New-Item -ItemType SymbolicLink -Path "$HOME\.gitconfig" -Target "$HOME\.configs\git\.gitconfig"
+New-Item -ItemType SymbolicLink -Path "$HOME\.ideavimrc" -Target "$HOME\.configs\rider\.ideavimrc"
